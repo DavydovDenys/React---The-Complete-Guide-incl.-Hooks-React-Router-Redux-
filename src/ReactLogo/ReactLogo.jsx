04d05a908567f56e08f80f89a73cf944f3872c0e
DavydovDenys <denvanrain@gmail.com>
2020-07-29 12:03:49 +0300

    refactoring

---
+++ src/ReactLogo/ReactLogo.jsx
@@ -0,0 +1,24 @@
+import logo from "../logo.svg";
+import React from "react";
+import style from "./ReactLogo.module.css"
+
+
+const ReactLogo = () => {
+  return (
+  <div className={style.App}>
+    <header className={style.AppHeader}>
+      <img src={logo} className={style.AppLogo} alt="logo"/>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+  );
+}
+
+export default ReactLogo;
